4e07a94927e5112036afc678c6e7386125c5b9cc
seanwbarry <sean.w.barry@gmail.com>
2015-08-30 15:06:45 +0800

    problem with the image in the timeline

---
+++ js/app.js
@@ -0,0 +1,38 @@
+/*$(function () { 
+
+//H1 JS
+	var interval=0;
+	setInterval(function() {
+		var height = 5.997;
+		var elem = document.getElementById('title');
+		elem.style.marginTop = (++interval%3)*(-height)+'rem';
+	}, 2000);
+
+
+});
+*/
+(function () {
+	$(function () {
+		var timelineAnimate = function (elem) {
+			return $(".timeline.animated .timelineRow").each(function(i) {
+				var bottomOfObject = $(this).offset().top + $(this).outerHeight();
+				var bottomOfWindow = $(window).scrollTop() + $(window).height();
+				console.log(bottomOfWindow);
+				console.log(bottomOfObject);
+				if(bottomOfWindow > bottomOfObject) {
+					return $(this).addClass("active");
+				} else {
+					return $(this).removeClass("active");
+				}
+			});
+		};
+		timelineAnimate();
+		return $(window).scroll(function () {
+			return timelineAnimate();
+		});
+	});
+}).call(this);
+
+
+//need to find a way to get the bottom of the window to match up to the bottom of the object...
+//need to get all of the other css out of the other one (use dev tools on each element in sources?? (check notes)...)
